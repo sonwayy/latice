3e6530a90a8c2b2e4b6409b284e36c1c20c37060
cemal <cemal24120@gmail.com>
2022-04-13 17:14:34 +0200

    added improvements to tile attribution and javaFX application

--- src/main/java/latice/application/LaticeApplicationConsole.java
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -14,24 +14,37 @@ public class LaticeApplicationConsole {
 		Tile blueBird = new Tile(Color.BLUE, Shape.BIRD);
 		Tile greenLeaf = new Tile(Color.GREEN, Shape.LEAF);
 		Tile redFlower = new Tile(Color.RED, Shape.FLOWER);
+		ArrayList<Tile> rackPlayer1 = new ArrayList<Tile>();
+		ArrayList<Tile> rackPlayer2 = new ArrayList<Tile>();
+		int tilesNumber = 3;
 		
-		ArrayList<Tile> tiles =new ArrayList<Tile>(Arrays.asList(redFlower, greenLeaf, redFlower));
+		ArrayList<Tile> tiles =new ArrayList<Tile>(Arrays.asList(blueBird, greenLeaf, redFlower));
 		
 		System.out.println("Hello Latice ! " + blueBird.getShape() + blueBird.getColor());
 		System.out.println("Hello Latice ! " + greenLeaf.getShape() + greenLeaf.getColor());
 		System.out.println("Hello Latice ! " + redFlower.getShape() + redFlower.getColor());
 		System.out.println("Hello Latice ! " + tiles.get(0).getShape() + tiles.get(0).getColor());
 		
-		//------------------------------Attribution des tuiles-----------------------------------
+		//------------------------------Tiles attribution-----------------------------------
 		Random random = new Random();
-		for(int i=0; i<2; i++) {
-			int randomNumber = random.nextInt(3);
-			Tile[] rackPlayer1 = {tiles.get(randomNumber), tiles.get(randomNumber)};;
+		for(int i=0; i<=tilesNumber; i++) {
+			if(! tiles.isEmpty()) {
+				int randomNumber = random.nextInt(tiles.size());
+				if(i%2 == 0) {
+					rackPlayer1.add(tiles.get(randomNumber));
+					System.out.println(rackPlayer1.get(0).getColor().toString() + rackPlayer1.get(0).getShape().toString());
+				} else {
+					rackPlayer2.add(tiles.get(randomNumber));
+					System.out.println(rackPlayer2.get(0).getColor().toString() + rackPlayer2.get(0).getShape().toString());
+				}
+				tiles.remove(randomNumber);
+				System.out.println(randomNumber);
+				System.out.println(rackPlayer1);
+				System.out.println(rackPlayer2);
+			}
+			
 		}
-		for(int i=0; i<2; i++) {
-			int randomNumber = random.nextInt(3);
-			Tile[] rackPlayer2 = {tiles.get(randomNumber), tiles.get(randomNumber)};
-		}
+		
 		
 	}
 
