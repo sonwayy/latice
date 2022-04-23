16db96f88760ec15b46927aefc38ff6334563312
mathi <mathi@192.168.1.16>
2022-04-23 16:05:32 +0200

    STEP 1 : ADD Deck and Rack classes with UPDATE LaticeApplicationConsole

--- src/main/java/latice/model/Rack.java
+++ src/main/java/latice/model/Rack.java
@@ -5,17 +5,24 @@ import java.util.ArrayList;
 public class Rack {
 	private ArrayList<Tile> listRackTile = new ArrayList<Tile>();
 	
-	public Rack(ArrayList<Tile> deck) {
+	public Rack(Deck deck) {
+		System.out.println("Il y a dans le rack : " + listRackTile.size() + " valeurs");
+		
 		for (int i = 0; i < 5; i++) {
-			int index = (int)(Math.random()*(deck.size()-0+1)+0);
-			listRackTile.add(deck.get(index));
-			System.out.println("l'indice de la tuile ajouté au rack est : " + index + " qui est la tuile " + deck.get(index));
+			int index = (int)(Math.random()*((deck.getListTile()).size()-0+1)+0); //(int)(Math.random()*(max-min+1)+min);
+			listRackTile.add((deck.getListTile()).get(index));
+			System.out.println("l'indice de la tuile ajouté au rack est : " + index + 
+					" qui est la tuile : couleur = " + (deck.getListTile()).get(index).getColor() + 
+					"  forme = " + (deck.getListTile()).get(index).getShape());
+			deck.getListTile().remove(index);
 			
 		}
 		
-		
+		System.out.println("Il y a dans le rack : " + listRackTile.size() + " valeurs");
 	}
+	
+	
+	// TODO add method(s) javafx
+
 }
 
-
-//(int)(Math.random()*(max-min+1)+min);  
