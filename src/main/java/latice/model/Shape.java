b72112ad8d2fef63de4939397f068c0e0a4e83ee
Mathis <Mathis@192.168.1.16>
2022-05-03 16:31:17 +0200

    UPDATE Shape with a new attribute

--- src/main/java/latice/model/Shape.java
+++ src/main/java/latice/model/Shape.java
@@ -1,5 +1,20 @@
 package latice.model;
 
 public enum Shape {
-	BIRD, DOLPHIN, FLOWER, LEAF, LIZARD, TORTOISE;
+	BIRD("bird"), 
+	DOLPHIN("dolphin"), 
+	FLOWER("flower"), 
+	feather("feather"), 
+	GECKO("gecko"), 
+	TURTLE("turtle");
+	
+	private String stringShape;
+	
+	Shape(String stringShape) {
+		this.stringShape = stringShape;
+	}
+	
+	public String getStringShape() {
+		return this.stringShape;
+	}
 }
