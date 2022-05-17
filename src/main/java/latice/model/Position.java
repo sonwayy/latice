bc278df80419ebbedaae95702406b0556400cd0d
Mathis <Mathis@192.168.1.16>
2022-05-17 09:17:09 +0200

    ADD Position | ADD attribute 'position' to Tile

---
+++ src/main/java/latice/model/Position.java
@@ -0,0 +1,21 @@
+package latice.model;
+
+public class Position {
+	private final Integer row;
+	private final Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+	
+	
+}
--- src/main/java/latice/model/Tile.java
+++ src/main/java/latice/model/Tile.java
@@ -3,10 +3,15 @@ package latice.model;
 public class Tile {
 	private final Color color;
 	private final Shape shape;
+	private Position position;
 	
 	public Tile(Color color, Shape shape) {
 		this.color = color;
 		this.shape = shape;
+	}
+	
+	public void setPosition(Position position) {
+		this.position = position;
 	}
 	
 	public Color getColor() {
@@ -15,5 +20,9 @@ public class Tile {
 	
 	public Shape getShape() {
 		return this.shape;
+	}
+	
+	public Position getPosition() {
+		return this.position;
 	}
 }
